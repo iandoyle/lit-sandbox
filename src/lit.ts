60137fc67633b17a55bac3edb268577a46b053bf
iandoyle <get@iandoyle.pro>
2020-09-22 18:15:51 -0400

    lit todo list

--- src/lit.ts
+++ src/lit.ts
@@ -1,8 +1,26 @@
 // Import lit-html
-import { html, render } from "lit-html";
 
+import { LitElement, html, customElement } from "lit-element";
+import './todos'
 // Define a template
-const myTemplate = (name: string) => html`<p>Get ${name}</p>`;
 
-// Render the template to the document
-render(myTemplate("Lit"), document.body);
+@customElement('x-app')
+class XApp extends LitElement {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return html`
+      <header>
+      <h1>Todos</h1>
+      </header>
+      <main>
+      <x-todos></x-todos>
+      </main>
+      <footer>
+      2020
+      </footer>
+    `
+  }
+}
---
+++ src/todo.service.ts
@@ -0,0 +1,35 @@
+import { Todo } from "./interfaces";
+
+export class TodoService {
+  getTodos() {
+    if (!localStorage.getItem('todos')) {
+      localStorage.setItem('todos', '[]');
+    }
+    const localstorage = JSON.parse(localStorage.getItem('todos') || '');
+    const todos: Todo[] = localstorage;
+    return todos ? todos : [];
+  }
+
+  updateTodo(todo: Todo, index: number) {
+    const todos = this.getTodos();
+    todos[index] = todo;
+    return this.saveTodos(todos);
+  }
+
+  deleteTodo(todo: Todo) {
+    const todos = this.getTodos();
+    const updatedTodos = todos.filter(t => t.value !== todo.value);
+    return this.saveTodos(updatedTodos);
+  }
+
+  createTodo(newTodo: string) {
+    const todos = this.getTodos();
+    const updatedTodos = [...todos, { completed: false, value: newTodo }];
+    return this.saveTodos(updatedTodos);
+  }
+
+  private saveTodos(updatedTodos: Todo[]) {
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+    return updatedTodos;
+  }
+}
