1451397942729db2c8984c9aa6076b730d75e248
iandoyle <get@iandoyle.pro>
2020-09-22 16:43:28 -0400

    scratch

--- lib/singleton.d.ts
+++
@@ -1,8 +0,0 @@
-declare class MySingleton {
-    static getInstance(): MySingleton;
-    private static instance;
-    private constructor();
-    greet(): string;
-}
-declare const singleton11: MySingleton;
-declare const singleton2: MySingleton;
