9892cb040b449ce6f46d240562ff6f68958f3bfa
iandoyle <get@iandoyle.pro>
2020-09-22 18:27:41 -0400

    lit todo list

--- lib/interfaces.d.ts
+++
@@ -1,4 +0,0 @@
-export interface Todo {
-    completed: boolean;
-    value: string;
-}
--- lib/todo.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Todo } from "./interfaces";
-export declare class TodoService {
-    getTodos(): Todo[];
-    updateTodo(todo: Todo, index: number): Todo[];
-    deleteTodo(todo: Todo): Todo[];
-    createTodo(newTodo: string): Todo[];
-    private saveTodos;
-}
